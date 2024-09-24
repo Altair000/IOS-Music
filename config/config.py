aad85b14c4acb1ace4b4daa5b795f7dc79d6cd04
Altair000 <raydiel_e@icloud.com>
2024-09-23 23:22:09 -0400

    Actualizar config.py

--- config/config.py
+++ config/config.py
@@ -1,10 +1,17 @@
 import os, telebot, time
-from tqdm import tqdm
 
 TOKEN = os.getenv('Token')
 bot = telebot.TeleBot(TOKEN)
 DIAWI = os.getenv('Diawi')
 
-def progress_bar(total):
-    for i in tqdm(range(total), desc="Progreso", unit="item"):
-        time.sleep(0.1)  # Simula un trabajo que toma tiempo
+def progress_bar(chat_id, bot):
+    total_steps = 10
+    msg = bot.send_message(chat_id, "Progreso: 0%")
+    
+    for i in range(total_steps):
+        time.sleep(0.5)  # Simula el tiempo que tarda el proceso
+        bot.edit_message_text(chat_id=chat_id, message_id=msg.message_id, 
+                              text=f"Progreso: {int((i + 1) / total_steps * 100)}%")
+    
+    bot.edit_message_text(chat_id=chat_id, message_id=msg.message_id, 
+                          text="Proceso completado.")
